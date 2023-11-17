e60eb5809d3590d413e123de06defec4ece4f55e
OpticUnderscoreS <alexskylarp@gmail.com>
2023-11-17 12:28:17 -0500

    Added case object
    Case object to be used for the game (self explainatory)

---
+++ casino/src/main/java/data/main/Case.java
@@ -0,0 +1,73 @@
+package data.main;
+
+// Case for game
+public abstract class Case {
+
+    public abstract int applyChanges(int currentValue); // Method to apply whatever the value of the case is to the current value
+
+    private String label; // Variable to be used to display a value on the graphics
+
+    public String getLabel() {
+        return label;
+    }
+
+    // Raw Constructor
+    private Case(String label) {
+        this.label = label; // Sets the label Variable to the specified label from main file
+    } // end Raw Constructor
+
+    // Creates a multiplier case
+    // I.E. x2 or x3
+    public static Case createMultiplier(int multiplier) {
+        return new Case("x" + multiplier) {
+
+            @Override
+            public int applyChanges(int currentValue) {
+                // Multiplies the Current value by the multiplier
+                return currentValue * multiplier;
+            }
+
+        };
+    }
+
+    // Creates a iterator case
+    // I.E. +1, -2, +2 etc.
+    public static Case createIterator(int iteration) {
+        // Sets the label for the case
+        String label;
+        // If the iteration is negative, leave it be, otherwise add a "+" in front of the label
+        if (iteration < 0) {
+            label = String.valueOf(iteration);
+        } else {
+            label = "+" + String.valueOf(iteration);
+        }
+
+        return new Case(label) {
+
+            @Override
+            public int applyChanges(int currentValue) {
+                // Adds/Removes the value of the case to the currentValue
+                return currentValue + iteration;
+            }
+
+        };
+
+    }
+
+    /** Creates a cupcake case, will be fixed later on
+     * @param full, boolean method to determine whether or not the cupcake is a full cupcake or a half cupcake
+     * @return
+     */
+    public static Case createCupcake(boolean full) {
+        return new Case("cupcake") {
+
+            @Override
+            public int applyChanges(int currentValue) {
+                // Unadded cupcake stuff, will get to after the gui is in a good state
+                throw new UnsupportedOperationException("Unimplemented cupcake handling");
+            }
+
+        };
+    }
+
+}
